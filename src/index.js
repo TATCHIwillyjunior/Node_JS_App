8633f661b20cfeb4b53d5a113957a15010500ddc
TATCHIwillyjunior <tatchi-feudjio.willy-junior@epita.fr>
2025-11-14 12:57:35 +0100

    new middleware

--- src/index.js
+++ src/index.js
@@ -1,35 +1,41 @@
-console.log("Node JS App Test File");
 
-const userOne = {
-    firstName: "Bog",
-    age: 27,
-    admin: false
-}
-
-userOne.age = 30
-console.log(userOne);
-
-const sport = ["tennis", "golf", "volleyball"]
-console.log(sport);
-
-
-// ES Modules 
-
-//const express = require("express")
+import { logMiddleware } from "./middleware/middleware.js"
 import express from "express"
+const app = express()
+const PORT = 3000
 
-const app = express();
-const PORT = 3000;
-app.use(express.json());
+const users = [
+	{ id: 1, name: "Alice" },
+	{ id: 2, name: "Bob" },
+	{ id: 3, name: "Charlie" },
+	{ id: 4, name: "Dave" },
+]
 
-app.get("/", (req, res) => {
-    res.json({
-	    firstName: "John",
-	    lastName: "Wick"
-    })
-});
+app.use(express.json())
+
+
+// middleware to log request body
+// app.use(async (req, res, next) => {
+// 	const date = new Date().toISOString()
+// 	console.log(`[${date}] ${req.method} ${req.url}`)
+
+	
+// 	const response = await fetch("https://jsonplaceholder.typicode.com/users/1")
+// 	const data = await response.json()
+// 	req.data = data
+// 	console.log(data)
+	
+// 	next()
+// })
+
+app.get("/", logMiddleware, (req, res) => {
+	res.json({ users, data })
+})
+
+app.post("/", (req, res) => {
+	console.log(req)
+})
 
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-});
-
+	console.log(`Server is running on http://localhost:${PORT}`)
+})
